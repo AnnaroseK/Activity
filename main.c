b66a5067d4cee31c1559a4448c2ea9353dcb3865
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-27 18:28:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,23 +5,6 @@
 #include <util/delay.h>
 
 int main(void)
-{   DDRB |= (1<<PB0);
-    DDRD &= ~(1<<PD0);
-    DDRD &= ~(1<<PD4);
-    PORTD |= (1<<PD0);
-    PORTD |= (1<<PD4);
-    // Insert code
-
-    while(1){
-        if(!(PIND & (1<<PD0))&&(!(PIND & (1<<PD4)))){
-            PORTB |= (1<<PB0);
-            _delay_ms(2000);
-        }
-        else{
-            PORTB &= ~(1<<PB0);
-            _delay_ms(2000);
-        }
-    }
-
-    return 0;
+{   
+    LED.c();
 }
