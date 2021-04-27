dfd5d53e4196dbce8f6658cd2c3cbf3ececb1d89
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-27 21:13:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,19 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "activity1.h"
+#include "activity2.h"
 
 int main(void)
 {   
     LED.c();
+    InitADC();
+    uint16_t temp;
+
+    while(1){
+        temp = ReadADC(0);
+        _delay_ms(200);
+    }
+
+    return 0;
 }
