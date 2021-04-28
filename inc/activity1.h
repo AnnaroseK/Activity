09244ed63e8d0353c12cf0d60221c938e5c1f1e8
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-28 18:35:10 +0530

    Add files via upload

---
+++ inc/activity1.h
@@ -0,0 +1,11 @@
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int LED(void);
+void port1();
+
+#endif // ACTIVITY1_H_INCLUDED
---
+++ inc/activity3.h
@@ -0,0 +1,10 @@
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+
+#include<avr/io.h>
+#include <util/delay.h>
+
+void port();
+void PWM(int);
+
+#endif // ACTIVITY3_H_INCLUDED
