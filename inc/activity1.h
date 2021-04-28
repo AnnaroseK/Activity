2ffd9db3dcaa34ac246f1b41da4fec0fbe5a693a
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-28 18:34:53 +0530

    Delete inc directory

--- inc/activity1.h
+++
@@ -1,6 +0,0 @@
-#ifndef ACTIVITY1_H
-#define ACTIVITY1_H
-
-int LED(void);
-
-#endif
--- inc/activity2.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ACTIVITY2_H__
-#define __ACTIVITY2_H__
-
-void InitADC();
-uint16_t ReadADC(uint8_t ch);
-
-#endif
