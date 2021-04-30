d399eb38a74d97771737f07a35ce70415a3b6e4a
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-30 12:26:07 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/**
+ * @file main.c
+ * @author Annarose K
+ * @brief Project to turn on chair heater in a car
+ * @version 0.1
+ * @date 2021-04-28
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include "activity1.h"
+#include "activity3.h"
+#include "activity2.h"
+
+/**
+ * @brief Main function where execution starts
+ *
+ * @return int
+ */
+
+int main(void)
+{   port();
+    //port1();
+    LED();
+    return 0;
+}
