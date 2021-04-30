8bf1d90b677489f987d8fff86dffdc055039dd42
AnnaroseK <69027899+AnnaroseK@users.noreply.github.com>
2021-04-30 12:22:31 +0530

    Add files via upload

---
+++ inc/activity4.h
@@ -0,0 +1,9 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
+
+void USARTInit(uint16_t);
+char USARTReadChar();
+void USARTWriteChar(char);
+int USART(int);
+
+#endif // ACTIVITY4_H_INCLUDED
